d506aa1a8cdf4eeb81175bdd75018882fb979d33
czyner <czyner7@gmail.com>
2023-10-13 09:00:06 +0800

    final calcu output

--- Calcu/action.php
+++ Calcu/action.php
@@ -4,19 +4,36 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initialscale=1.0">
-        <title>Document</title>
+        <title>Calculator</title>
     </head>
     <body>
-
-        The  <?php
-        echo (int)$_POST['n1'];
-        ?>
-        and  <?php
-        echo (int)$_POST['n2'];
-        ?> 
-        is 
-
-
-
+        <?php
+        print("Simple Calculator <br>");
+        $n1 = $_POST['n1']; $n2 = $_POST['n2'];
+        $operand = $_POST['Operand'];
+        $result = ''; $resultname = '';
+        if (is_numeric($n1) && is_numeric($n2)) {
+            if($operand == "Add"){
+                $result = $n1 + $n2;
+                $resultname = 'Sum';
+            } elseif($operand == "Subtract"){
+                $result = $n1 - $n2;
+                $resultname = 'Difference';
+            } elseif($operand == "Multiply"){
+                $result = $n1 * $n2;
+                $resultname = 'Product';
+            } elseif($operand == "Divide"){
+                $result = $n1 / $n2;
+                $resultname = 'Quotient';
+            }
+        }?>
+        The <?php echo ($resultname);?>
+        of <?php echo ($n1);?>
+        and <?php echo ($n2);?>
+        is equal to <?php echo ($result);?>
+        <form action="index.php" method="post">
+            <br>
+            <button type="submit">Back</button>
+        </form>
     </body>
 </html>
--- Calcu/index.php
+++ Calcu/index.php
@@ -4,21 +4,25 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initialscale=1.0">
-        <title>Document</title>
+        <title>Calculator</title>
     </head>
     <body>
-        <form action="action.php" method="post">
-            <label for="n1">First Number</label>
-            <input name="n1" id="n1" type="number">
-
-            <label for="n2">Second Number:</label>
-            <input name="n2" id="n2" type="number">
-            <p name="result" id="result"></p>
-
-            <button type="add">Addition</button>
-            <button type="subtract">Subtraction</button>
-            <button type="multiply">Multiplication</button>
-            <button type="divide">Division</button>
+    <?php
+    print("Simple Calculator <br>");
+    ?>
+    <form action="action.php" method="post">
+            <label for="n1">First Number:</label>
+            <input name="n1" id="n1" type="number" />
+            <br>
+            <label for="second number">Second Number:</label>
+            <input name="n2" id="n2" type="number" />
+            <br>
+            <input type="submit" name="Operand" value="Add" />
+            <input type="submit" name="Operand" value="Subtract" />
+            <input type="submit" name="Operand" value="Multiply" />
+            <input type="submit" name="Operand" value="Divide" />
         </form>
     </body>
 </html>
+
+
